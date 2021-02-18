d561d761289c8d1eb7848f950e71a72d5738f7dc
bettyjohb <bjhb.goomail@gmail.com>
2021-02-18 08:19:21 -0800

    Initial sfw5-bg-petclinic project created with Initializr, and comprehensive .gitignore

---
+++ src/main/java/guru/springframework5/sfw5bgpetclinic/Sfw5BgPetclinicApplication.java
@@ -0,0 +1,14 @@
+package guru.springframework5.sfw5bgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sfw5BgPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sfw5BgPetclinicApplication.class, args);
+		System.out.println("Up and running!");
+	}
+
+}
---
+++ src/test/java/guru/springframework5/sfw5bgpetclinic/Sfw5BgPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework5.sfw5bgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sfw5BgPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
