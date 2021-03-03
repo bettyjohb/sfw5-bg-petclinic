4e62a2a3027421b8688ddd4c318036ebb47e0258
bettyjohb <bjhb.goomail@gmail.com>
2021-03-03 08:46:49 -0800

    Update up and running message to test git push

--- pet-clinic-web/src/main/java/guru/springframework5/sfw5bgpetclinic/Sfw5BgPetclinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework5/sfw5bgpetclinic/Sfw5BgPetclinicApplication.java
@@ -8,7 +8,7 @@ public class Sfw5BgPetclinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sfw5BgPetclinicApplication.class, args);
-		System.out.println("Up and running!");
+		System.out.println("Up and running!!!");
 	}
 
 }
