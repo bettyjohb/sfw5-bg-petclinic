38e797e84702141c08371ec8a5988738b7b5372e
bettyjohb <bjhb.goomail@gmail.com>
2021-03-16 12:36:51 -0700

    Create VetController and index.html for vet folder.

--- pet-clinic-web/src/main/java/guru/springframework5/sfw5bgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework5/sfw5bgpetclinic/controllers/VetController.java
@@ -1,11 +1,11 @@
 //***************************************************************************
-//Class:  IndexController 
+//Class:  VetController 
 //
-//Controller for main index page (identified by @Controller / not extension).
+//Primary Controller for vet related pages (identified by @Controller / not extension).
 //
 //When an HTTP request arrives at server, will be handled by Spring provided 
 //DispatcherServlet (front controller) that uses Handler Mapping to determine 
-//which Controller to invoke (i.e. IndexController, etc.).  
+//which Controller to invoke (i.e. VetController, etc.).  
 //
 //@RequestMapping is used to map the HTTP Request Path to specific method on 
 //the controller (for instance, can say all URL for that Web App ending with
@@ -14,10 +14,10 @@
 //tell where to find file and to add ".jsp."  When Thymeleaf, default looks in 
 //templates and finds hello.html.
 //
-// Key requirements:
-// #1 - @Controller - Tell Spring this is a Spring MVC Controller to be instantiated
-// #2 - @RequestMapping - Inform Spring this is a controller method that can be invoked
-//                        to handle specific incoming HTML request.
+//Key requirements:
+//#1 - @Controller - Tell Spring this is a Spring MVC Controller to be instantiated
+//#2 - @RequestMapping - Inform Spring this is a controller method that can be invoked
+//                      to handle specific incoming HTML request.
 //*************************************************************************** 
 package guru.springframework5.sfw5bgpetclinic.controllers;
 
@@ -25,15 +25,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller      // #1 - Tell Spring this is a Spring MVC Controller to be instantiated. 
-public class IndexController {
+public class VetController {
 
 	// #2 - Tell Spring this is a Controller method to handle HTTP requests.
-	//      Specifically, this method handles if user ends URL with empty string, /, index, or index.html)
+	//      Specifically, this method handles if user ends URL with /vets, /vets/index or /vets/index.html.
 	//      If you run the app will see RequestMappingHandlerMapping list these 4 types handled.
 	//
-    //      Returns core name of Thymeleaf template - looks in src/main/resources/templates by default.  
-	@RequestMapping({"", "/","index","index.html"})	 
-	public String index() {
-		return ("index");       // Spring looks in src/main/resources in templates by default for index.html
-	}
-}  // end IndexController
+	//      - Request vets from service to add to Model.  
+    //      - Returns core name of Thymeleaf template. 
+    @RequestMapping({"/vets", "/vets", "/vets/index", "/vets/index.html"})
+	public String listVets() {
+    	return "vets/index";  // Spring looks in templates in vets folder for index.html since Thymeleaf. 
+    	                      // If JSP, ViewResolver would provide pre/suffix to build full jsp file/path. 
+    }
+    
+}  // end class VetController
+
