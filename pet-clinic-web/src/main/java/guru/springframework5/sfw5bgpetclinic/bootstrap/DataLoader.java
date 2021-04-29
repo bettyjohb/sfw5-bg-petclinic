f7d1835ade30c017ded5126edd455abaa112d3d7
bettyjohb <bjhb.goomail@gmail.com>
2021-04-29 12:20:40 -0700

    Added Bootstrap for data loading (DataLoader class).  Closes #22.

---
+++ pet-clinic-web/src/main/java/guru/springframework5/sfw5bgpetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,76 @@
+// Class:       DataLoader
+// Implements:  CommandLineRunner (Spring Boot specific interface says a bean should run() if within Spring project)
+// 
+// DataLoader.run() will be executed on startup.  Spring and the DataLoader bootstrap class is responsible for determining the current method
+// of persistence being utilized (Map [HashMap], Spring Data JPA, or straight up JPA).  Based on this, it will load (mock) data 
+// utilizing the appropriate service (i.e., OwnerServiceMapImpl if using Map persistence).  This way, on startup, we have known mock data 
+// to utilize to validate the system.
+// 
+// Recall, there is a primary CRUD service interface from which multiple IMPL types implement (MAP, JPA, etc.).  The appropriate IMPL is 
+// utilized based on Spring profiles (to be implemented).  [Similar to properties file for Regis class that determined which IMPL to 
+// use.  Based on the profile, the data loader will use the correct service (i.e., OwnerServieMapImpl) to input mock data it creates. 
+//
+// NOTE:  Id management will be an issue (particularly for Maps).  Currently, can input a duplicate ID into the map.  Therefore, will
+// want to move the responsibility of ID generation to the Map classes.  This is like DB that generate IDs. 
+package guru.springframework5.sfw5bgpetclinic.bootstrap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import guru.springframework5.sfw5bgpetclinic.model.Owner;
+import guru.springframework5.sfw5bgpetclinic.model.Vet;
+import guru.springframework5.sfw5bgpetclinic.services.OwnerService;
+import guru.springframework5.sfw5bgpetclinic.services.VetService;
+import guru.springframework5.sfw5bgpetclinic.services.map.OwnerServiceMapImpl;
+import guru.springframework5.sfw5bgpetclinic.services.map.VetServiceMapImpl;
+
+@Component   // Make this a Spring bean loaded into Spring Context, therefore Spring sees CommandLineRunner and executes with run(). 
+public class DataLoader implements CommandLineRunner {
+
+	// Reference to interface types
+	private final OwnerService ownerService;
+	private final VetService vetService;      
+	
+	// Since @Component, component scan will instantiate at startup thereby calling default constructor. 
+	public DataLoader() {
+		// Utilize existing services the implement the CRUD operations to initilze mock data.
+		// BAD - WILL USE DI (not new) in future change. 
+		ownerService = new OwnerServiceMapImpl();
+		vetService = new VetServiceMapImpl();
+	}  // end Constructor
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Owner owner1 = new Owner();
+		owner1.setId(1L);
+		owner1.setFirstName("Michael");
+		owner1.setLastName("Weston");
+		
+		ownerService.save(owner1);
+		
+		Owner owner2 = new Owner();
+		owner2.setId(2L);
+		owner2.setFirstName("Fionna");
+		owner2.setLastName("Glenanne");
+		
+		ownerService.save(owner2);
+		
+		System.out.println("Loaded owners....");
+		
+		Vet vet1 = new Vet();
+		vet1.setId(1L);
+		vet1.setFirstName("Sam");
+		vet1.setLastName("Axe");
+		
+		vetService.save(vet1);
+		
+		Vet vet2 = new Vet();
+		vet2.setId(2L);
+		vet2.setFirstName("Jessie");
+		vet2.setLastName("Porter");
+		
+		vetService.save(vet2);
+		
+		System.out.println("Loaded vets....");
+	}  // end run()
+	
+}  // end DataLoader
