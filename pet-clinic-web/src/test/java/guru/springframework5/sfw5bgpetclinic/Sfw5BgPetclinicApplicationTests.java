fddc49fc8321dafd39569bef950e00dd6d7873cb
bettyjohb <bjhb.goomail@gmail.com>
2021-10-27 15:30:38 -0700

    Update web and data modules' pom.xml files for JUnit 5.

--- pet-clinic-web/src/test/java/guru/springframework5/sfw5bgpetclinic/Sfw5BgPetclinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework5/sfw5bgpetclinic/Sfw5BgPetclinicApplicationTests.java
@@ -1,11 +1,16 @@
 package guru.springframework5.sfw5bgpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+//@ExtendWith(SpringExtension.class)  // Replaces JUnit4 @RunWith(SpringRunner.class) to bring in Context.
+									   // Included within @SpringBootTest
 @SpringBootTest
 class Sfw5BgPetclinicApplicationTests {
 
+	// This just brings up Spring Context for tests because of ExtendWith(SpringExtension.class). 
 	@Test
 	void contextLoads() {
 	}
