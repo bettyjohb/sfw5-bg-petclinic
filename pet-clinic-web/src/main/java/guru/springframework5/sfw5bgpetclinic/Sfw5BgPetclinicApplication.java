a77ac6365165a1836791cf893f0dcfefd6b81ff3
bettyjohb <bjhb.goomail@gmail.com>
2023-07-31 12:12:00 -0700

    bring source current

--- pet-clinic-web/src/main/java/guru/springframework5/sfw5bgpetclinic/Sfw5BgPetclinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework5/sfw5bgpetclinic/Sfw5BgPetclinicApplication.java
@@ -9,7 +9,7 @@ public class Sfw5BgPetclinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sfw5BgPetclinicApplication.class, args);
-		System.out.println("Petclinic is now up and running!!!");
+		System.out.println("Petclinic is now up and running!");
 	}
 
 }
